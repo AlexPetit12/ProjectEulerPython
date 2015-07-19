18a02a7f91eef8ccf779510201bf87510e32e10c
Alex <alexpetit12@hotmail.com>
2015-07-19 11:06:56 -0400

    Problem 005 solution

---
+++ 005 - Smallest multiple/main.py
@@ -0,0 +1,13 @@
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+
+"""
+No need to program for this problem. All I did was finding the least common multiple of
+all the numbers from 1 to 20. 
+
+I obtained the prime factorization 2 x 2 x 2 x 2 x 3 x 3 x 5 x 7 x 11 x 13 x 17 x 19 = 232792560
+which is the final answer.
+"""
